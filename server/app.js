cd687b8b377da7bbf2717565fe222f047432661b
Nikolay <kolya.kuzmichev.82@mail.ru>
2022-11-28 22:43:10 +0300

    feate: update

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
+const fs = require("fs")
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT||require("config").get("PORT")
-
+app.get("/verbs",(req,res)=>{
+    const content = fs.readFileSync("./data/a1/verbs.json", 'utf8');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+console.log(content)
+// res.type('text/plain')
+    res.json(content)
+})
 app.listen(PORT,()=>console.log(`Server start on port ${PORT}`))
